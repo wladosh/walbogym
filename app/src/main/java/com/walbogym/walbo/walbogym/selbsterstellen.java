1d5ced7e2551dbbbfa570b4304deb053d13da37e
wladi0 <wl.udod@gmail.com>
2019-01-20 22:58:24 +0100

    update

---
+++ app/src/main/java/com/walbogym/walbo/walbogym/selbsterstellen.java
@@ -0,0 +1,13 @@
+package com.walbogym.walbo.walbogym;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class selbsterstellen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_selbsterstellen);
+    }
+}
